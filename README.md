cec800d407766c54d8674ae40d4013dae5fd4989
kmftcy <kmftangchaoyang@gmail.com>
2023-09-30 17:27:54 +0800

    file exist check

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # CIE6004_assignment1
 
 This a project for my CIE6004 course's 1st assignment.
+
+## Environment
+
+- python 3.9.13
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+# Env check
+# check dataset file exists
+import os.path
+dataset_path = 'garbage_classification.zip'
+if not os.path.isfile(dataset_path):
+    print("The dataset is not found. Please put it in the root of the directory and rename it as \"garbage_classification.zip\".")
+    exit()
+
 print("hello world")
